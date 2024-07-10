db904295401f65c7955ea7703f837f90119bea95
aaliyahwithadoublea <111052111+aaliyahwithadoublea@users.noreply.github.com>
2024-07-10 20:35:23 +0100

    another change

--- checkout.js
+++ checkout.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const nav = document.querySelector('.nav');
 
+    const fur = document.getElementById
+
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('open');
     });
