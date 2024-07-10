bb4059fb027215f546945111527c7750a85c7fe6
aaliyahwithadoublea <111052111+aaliyahwithadoublea@users.noreply.github.com>
2024-07-10 20:41:06 +0100

    commiting again

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById('hamburger');
     const nav = document.querySelector('.nav');
-
+const gut = console.log("it's working")
     hamburger.addEventListener('click', function() {
         nav.classList.toggle('open');
     });
